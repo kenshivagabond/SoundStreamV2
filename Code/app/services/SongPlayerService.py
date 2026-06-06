c288bc17771cc49dfe76550da43aeaf15be211bd
kenshivagabond <naekenshi@gmail.com>
2026-06-06 14:44:44 +0200

    Avancement

--- Code/app/services/SongPlayerService.py
+++ Code/app/services/SongPlayerService.py
@@ -7,6 +7,7 @@ import datetime
 import platform
 import os
 from app import app
+import ping3
 
 ts = TimeTableService()
 
@@ -16,68 +17,10 @@ class SongPlayerService:
     
     def __init__(self) :
         self.spdao = SongPlayerDAO()
-    
-   
-    def ping(self,ip):
-        """
-        Checks if a song player is reachable on the network.
-
-            Args:
-                ip (str): The IP address of the song player to ping.
-
-            Returns:
-                bool: True if the player responds, False otherwise.
-        """
-        try :
-            if platform.system() == 'Windows':
-                command = ["ping", "-n", "1", "-w", "1000", ip] # Ping command for Windows with 1 echo request and 1 second timeout (in milliseconds)
-                # Or use : command = ["Test-NetConnection","-ComputerName", ip] #Windows specific ping command (Default timeout is 1 second & 1 echo)
-
-                result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                return result.returncode == 0
-
-            else:  # For Unix/Linux/Mac
-                command = ["ping","-c","1","-w","1",ip] # Ping command for Linux with 1 echo request and 1 second timeout (in seconds)
-
-                result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                return result.returncode == 0
-
-        except subprocess.CalledProcessError:
-            # If subprocess fails, return False
-            return False
-        except Exception:
-            # In case of any other exception, return False
-            return False
+  
         
 
-    def changeState(self, id_song_player):
-        """
-        Checks the network status of a player and updates its state in the database.
-
-            Args:
-                id_song_player (int): The unique ID of the song player to check.
-        """
-        # Define the two possible states: "ONLINE" and "OFFLINE"
-        state_online = "ONLINE"
-        state_offline = "OFFLINE"
-    
-        # Retrieve the song player associated with the given id from the database
-        song_player = self.spdao.findByID(id_song_player)
-        if song_player:
-            # Get the Ip adress of the song player
-            ip=song_player.IP_adress
-    
-            # Check if the IP is reachable (ping the IP)
-            if self.ping(ip) == False:
-                # If the IP is not reachable, update the player's state to "OFFLINE"
-                self.spdao.UpdateState(state_offline, id_song_player)
-            else:
-                # If the IP is reachable, update the player's state to "ONLINE"
-                self.spdao.UpdateState(state_online, id_song_player)
-        else :
-            return
-
-
+   
     def deleteSongPlayer(self,id_song_player):
         """
         Deletes a song player from the system and the database.
@@ -110,11 +53,12 @@ class SongPlayerService:
                 list[dict]: A list of song players as dictionaries with refreshed network statuses.
         """
         # Fetch initial list of players to identify who needs to be checked
+        self.spdao.findDevices()
         players = self.spdao.findAllByOrganisationInBd(id_orga)
 
         # Update each player's status by pinging their IP address
         for player in players:
-            self.changeState(player.id_player)
+            spdao.UpdateState(player.ip);
 
         # Retrieve the updated objects from the database after synchronization
         updated_players = self.spdao.findAllByOrganisationInBd(id_orga)
@@ -186,25 +130,8 @@ class SongPlayerService:
                 except Exception as e:
                     print(f"Erreur synchro {subfolder} vers {ip} : {e}")
                     
-    def sync_to_device(self, ip, device_name):
-        """ Envoie les fichiers vers la vm Debian distante dans des dossiers séparés """
-
-        threading.Thread(target=self.run_sync, args=(ip, device_name)).start()
-
-    def remote_play_playlist(self, ip, device_name, playlist_name):
-        """ 
-        Commande le MPD distant pour charger et lire une playlist précise 
-        """
-        remote_cmd = f"mpc clear && mpc load {playlist_name} && mpc play"
-        
-        ssh_cmd = ["ssh", f"{device_name}@{ip}", remote_cmd]
-        
-        try:
-            subprocess.run(ssh_cmd, check=True)
-            print(f"Lecture de {playlist_name} lancée sur {ip}")
-        except Exception as e:
-            print(f"Erreur lors de l'envoi du chargement de la playlist : {e}")
-
+            threading.Thread(target=self.run_sync, args=(ip, device_name)).start()
+    
     def run_check(self):
         print("Scheduler démarré...")
         while True:
@@ -237,9 +164,3 @@ class SongPlayerService:
         threading.Thread(target=self.run_check, daemon=True).start()
 
 
-####################################
-## Calendar view & Timetable view ##
-####################################
-
-def trackStartingAt(self, day_name: str, hour: int, minute: int) -> list:
-    """"""
